79457649bca435bb68312eb1c366fb783123a6bb
Jiwoo-Lim <jiwoo.lim@ibm.com>
2021-09-29 17:07:41 -0400

    debug

--- dev/com.ibm.ws.install/src/com/ibm/ws/install/internal/Director.java
+++ dev/com.ibm.ws.install/src/com/ibm/ws/install/internal/Director.java
@@ -852,7 +852,7 @@ public class Director extends AbstractDirector {
                         ESAAsset esaa = ((ESAAsset) installAsset);
                         if (esaa.isPublic()) {
                             log(Level.FINE, installAsset.installedLogMsg());
-                            log(Level.FINE, "ESA file installed from : " + esaa.getAsset().getAbsolutePath());
+                            log(Level.FINE, "ESA file is installed from : " + esaa.getAsset().getAbsolutePath());
                         }
                     } else {
                         log(Level.FINE, installAsset.installedLogMsg());
